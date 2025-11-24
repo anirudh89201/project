43b5392c02cd783c3aa4f9ba2e2611caf3456af6
AnirudhReddy <44012589+anirudh89201@users.noreply.github.com>
2025-11-24 21:39:12 +0530

    Initial Commit

---
+++ frontend/project-frontend/app/_layout.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import {Stack} from "expo-router"
+import React from 'react';
+const MainLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+      name='stack-screens/home'
+      options={{
+        headerShown:false,
+      }}
+      />
+    </Stack>
+  )
+}
+
+export default MainLayout
