2b9d1140a65df9fabc80e352b06a47000f4a9e5b
AnirudhReddy <44012589+anirudh89201@users.noreply.github.com>
2025-12-08 15:35:51 +0530

    Implemented DockerFile..

--- backend/routes/resume.routes.js
+++ backend/routes/resume.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import { GetUserReport, handleUpload, InsertReport} from "../controllers/resume.controller.js";
+import {handleUpload} from "../controllers/resume.controller.js";
 import { optionalJWT } from "../middleware/optionalJWT.js";
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
